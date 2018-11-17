afebf8199f7419647e4ab11d036ae9b28b612acd
Talicode <talisancreations@gmail.com>
2018-11-17 13:10:58 -0800

    Building message passing JS/Node

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 // Import the necessary extensibility types to use in your code below
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as $ from 'jquery';
 
 /* NOT WORKING for now, come back to later
 const shelties = {
@@ -24,14 +23,11 @@ export function activate(context: vscode.ExtensionContext) {
     
     // Path to media resources on disk
     const mediaPath = vscode.Uri.file(path.join(context.extensionPath, 'media'));
-    // Initialize media resources
     // TODO base all the media resources on the above mediaPath
-    const boneSheltie = vscode.Uri.file(path.join(context.extensionPath, 'media', 'bone.gif'));
-    const boneSheltieSrc =  boneSheltie.with({scheme: 'vscode-resource'}).toString();
-    // Javascript file
-    // TODO link the javascript file here
-    const jScripts = vscode.Uri.file(path.join(context.extensionPath, 'src', 'scripts.js'));
-    const jScriptsSource = jScripts.with({scheme: 'vscode-resource'}).toString();
+    // Initialize media resources
+    const twitterLogo = vscode.Uri.file(path.join(context.extensionPath, 'media', 'Twitter_Logo_Blue.svg'));
+    const twitterLogoSrc =  twitterLogo.with({scheme: 'vscode-resource'}).toString();
+    // TODO link the javascript & css file
 
     // WEBVIEW SETUP
     // Only allow a single webview to exist at a time. If it's in the background, then bring it to the foreground
@@ -65,7 +61,7 @@ export function activate(context: vscode.ExtensionContext) {
             }
             );
 
-            currentPanel.webview.html = getWebviewContent(boneSheltieSrc,jScriptsSource);
+            currentPanel.webview.html = getWebviewContent(twitterLogoSrc);
 
             /* Not working for now, testing URI conversions
             // Update contents based on view state changes
@@ -92,6 +88,12 @@ export function activate(context: vscode.ExtensionContext) {
                 currentPanel = undefined;
             }, null, context.subscriptions);
         }
+
+        // Listener for messages posted back to webview
+        currentPanel.webview.onDidReceiveMessage(e => {
+            vscode.window.showInformationMessage('Recieved a message from client JS!');
+            console.log(e);
+        });
     }));
 
     // Pass message from an extension to a webview to refactor (reduce the count)
@@ -113,94 +115,109 @@ function updateWebviewBySheltie(panel: vscode.WebviewPanel, sheltieAction: keyof
 }
 */
 
-function getWebviewContent(imgSrc: string, scriptSrc: string){
+function getWebviewContent(imgSrc: string){
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Twitter Talk</title>
+        <!-- TODO: Change to external file. This doesn't work, embedding styles for now 
+        <link rel="stylesheet" href="./style.css"> -->
+        <style>
+            h1 {
+                color: blue;
+                text-align: center;
+            }
+            h2 {
+                text-align: center;
+                margin: 0px;
+                padding: 10px;
+            }
+            img {
+                width: 80%;
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            div.box {
+                width: 40%;
+                margin: 0 auto;
+                padding: 10px;
+                border: 1px solid #38A1F3;
+            }
+            textarea {
+                width: 100%;
+                display: inline-block;
+                margin: 0px;
+                padding: 10px;
+                box-sizing: border-box;
+            }
+            input {
+                width: 100%;
+                font-size: 150%; 
+                display: inline-block;
+                background-color: #38A1F3;
+                border: none;
+                padding: 10px;
+                box-sizing: border-box;                
+            }
+            button {
+                width: 100%;
+                font-size: 150%; 
+                display: inline-block;
+                background-color: #38A1F3;
+                border: none;
+                padding: 10px;
+                box-sizing: border-box;
+            }
+            p {
+                width: 40%;
+                margin: 0 auto;
+                padding: 10px;
+            }
+        </style>
+
+        <!-- Not working just yet, trying to get external scripts to load
+        <script src="./scripts.js" type="text/javascript"></script>
+        -->
 
         <!-- TODO: A more sane content security policy, but it breaks embedded scripts
         <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src vscode-resource:; style-src vscode-resource:;">
         -->
+
     </head>
     <body>
-        <div id="mainform">
-        <h2>Send a Tweet!</h2>
-        <!-- Form Div starts here -->
-        <form id="form">
-            <h3>Contact Form</h3>
-            <p id="returnmessage"></p>
-            <label>Name: <span>*</span></label>
-            <input type="text" id="name" placeholder="Name"/>
-            <label>Email: <span>*</span></label>
-            <input type="text" id="email" placeholder="Email"/>
-            <label>Contact No: <span>*</span></label>
-            <input type="text" id="contact" placeholder="10 digit Mobile no."/>
-            <label>Message:</label>
-            <textarea id="message" placeholder="Message......."></textarea>
-            <input type="button" id="submit" value="Send Message"/>
-        </form>
+
+        <div class="box">
+            <h2>Send a Tweet!</h2>
+            <img src="${imgSrc}" width="300" />
+
+            <!-- Form starts here -->
+            <h3>Message:</h3>
+            <textarea id="message" placeholder="140 characters or fewer"></textarea>
+            <button onClick="submit()">Send Message</button>
+
+            <!-- Form for testing Node.js value passing -->
+            <form method="post" action="/">
+                <input type="hidden" name="testVariable" value="this is passing from the webview to Node.js">
+                <input type="submit" name="submit" value="Submit"
+            </form>
         </div>
 
-        <script>
-        $(document).ready(function() {
-            $("#submit").click(function() {
-            var name = $("#name").val();
-            var email = $("#email").val();
-            var message = $("#message").val();
-            var contact = $("#contact").val();
-            $("#returnmessage").empty(); // To empty previous error/success message.
-            // Checking for blank fields.
-            if (name == '' || email == '' || contact == '') {
-            alert("Please Fill Required Fields");
-            } else {
-            // Returns successful data submission message when the entered information is stored in database.
-            $.post("", { // TODO MAKE POST FUNCTION DO SOMETHING
-            name1: name,
-            email1: email,
-            message1: message,
-            contact1: contact
-            }, function(data) {
-            $("#returnmessage").append(data); // Append returned message to message paragraph.
-            if (data == "Your Query has been received, We will contact you soon.") {
-            $("#form")[0].reset(); // To reset form fields on success.
-            }
-            });
-            }
-            });
-        });   
+        <h2>Message is:</h2>
+
+        <p id="message-text">No message entered</p>
+    
+        <script language="javascript">
+        const text = document.getElementById('message-text');
+        //TODO sanitize content
+        //TODO limit the length to 140 characters
+        function submit(){
+            text.textContent = document.getElementById("message").value;
+        }
         </script>
 
-        <!-- below contents are for testing purposes to see what I break while working -->
-        <img src="${imgSrc}" width="300" />
-
-        <h1 id="lines-of-code-counter">0</h1>
-
-        <!-- Not working just yet, trying to get external scripts to load -->
-        <script src="${scriptSrc}" type="text/javascript"></script>
-
-        <script>
-            const counter = document.getElementById('lines-of-code-counter');
-                
-            let count = 0;
-            // TODO: This count seems to increment forever, want it to stop at 100
-            setInterval(() => {
-                counter.textContent = count++;
-            }, 100);
-
-            // Handle the message inside the window
-            window.addEventListener('message', event => {
-                const message = event.data // JSON data sent
-                switch(message.command){
-                    case 'refactor':
-                        count = Math.ceil(count * 0.5);
-                        counter.textContent = count;
-                        break;
-                }
-            })
-        </script>
     </body>
     
     </html>`;
